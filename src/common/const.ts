167fb6551ea8675d65ee0846a2aa83400fdf77d5
sunjingfeng <790964731@qq.com>
2024-08-26 16:01:45 +0800

    feat: change

--- src/common/const.ts
+++ src/common/const.ts
@@ -1,6 +1,6 @@
 export const CONFIG = {
   appName: '系统配置管理',
-  authorityRoute: false,
+  authorityRoute: true,
   mode: import.meta.env.MODE,
   basePath: import.meta.env.VITE_BASE_PATH,
   baseUrl: import.meta.env.VITE_BASE_URL,
